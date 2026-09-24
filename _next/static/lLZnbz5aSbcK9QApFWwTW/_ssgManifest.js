b14c9e97e5f02fcdb4ad7a9275c387c8658399ea
pzagardedukic <petra.zagar.dedukic@gmail.com>
2026-09-24 10:30:03 +0200

    Objava: slicica videa je v HTML

--- _next/static/lLZnbz5aSbcK9QApFWwTW/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fcenik\u002F[slug]","\u002Fdogodki\u002F[slug]","\u002Fprojekti\u002F[slug]","\u002Fzaposlitev\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
